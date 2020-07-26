1d549bba5575f61d61728137a5e700f8703aa998
PedrooLska <pedrolucas81@hotmail.com>
2020-07-26 11:49:45 -0300

    Add Syntax Sugar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,6 @@
 let result = document.querySelector('#result');
 let count = 0
 
-const addCount = () => {
-    showResult(++count);
-}
-
-const decrementCount = () => {
-    showResult(--count);
-}
-
-const showResult = (count) => {
-    result.innerHTML = count;
-}
+const addCount = () => showResult(++count);
+const decrementCount = () => showResult(--count);
+const showResult = (count) => result.innerHTML = count;
